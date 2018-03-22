b7948fe2659fb0c6483d8ac35e9e452913c570dd
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2018-03-22 22:19:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const logchanneljcl = "log";
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
+});
 const token = 'NDI2MzEyODM1NzQ5MTE3OTcy.DZUKUA.U-aBxhurxk5iTyOI6ic8zcxUO-Y';
 client.on('ready', () => {
   console.log(`Success!`);
