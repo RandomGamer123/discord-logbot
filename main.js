cb450678f01a659479697930abcf60d6422d135a
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2018-03-22 22:02:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,72 +10,68 @@ client.on('ready', () => {
 });
 
 client.on('messageDelete', message => {
-  var logchannel = 0;
-  if (message.guild.id == ramtid) {
-    logchannel = logchannelramt;
-  } else if (message.guild.id == jclid) {
-    logchannel = logchanneljcl;
-  } else {
-    try { 
-      message.channel.send('An error has happened, please report this to RandomGamer123 #5222 immediately');
+  try:var logchannel = 0;
+    if (message.guild.id == ramtid) {
+      logchannel = logchannelramt;
+    } else if (message.guild.id == jclid) {
+      logchannel = logchanneljcl;
+    } else {
+      try { 
+        message.channel.send('An error has happened, please report this to RandomGamer123 #5222 immediately');
+      }
+      catch(err) {
+      }
     }
-    catch(err) {
+    var channelobj = message.guild.channels.find("name", logchannel);
+      var desc = "Message sent by <@" + message.member.id +"> deleted in <#" + message.channel.id + ">";
+      channelobj.send({embed: {
+        color: 16711680,
+        title: "Message Deleted",
+        description: desc,
+        fields: [{
+            name: "Original Message:",
+            value: message.content
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: "User id of original message sender: " + message.member.id
+        }
+      }
+    });
+  });
+  client.on('messageUpdate', (message, emessage) => {
+    var logchannel = 0;
+    if (message.guild.id == ramtid) {
+      logchannel = logchannelramt;
+    } else if (message.guild.id == jclid) {
+      logchannel = logchanneljcl;
+    } else {
+        message.channel.send('An error has happened, please report this to RandomGamer123 #5222 immediately');
     }
-  }
   var channelobj = message.guild.channels.find("name", logchannel);
-    var desc = "Message sent by <@" + message.member.id +"> deleted in <#" + message.channel.id + ">";
-    channelobj.send({embed: {
-      color: 16711680,
-      title: "Message Deleted",
-      description: desc,
-      fields: [{
-          name: "Original Message:",
-          value: message.content
-        },
-      ],
-      timestamp: new Date(),
-      footer: {
-        text: "User id of original message sender: " + message.member.id
-      }
-    }
-  });
-});
-client.on('messageUpdate', (message, emessage) => {
-  var logchannel = 0;
-  if (message.guild.id == ramtid) {
-    logchannel = logchannelramt;
-  } else if (message.guild.id == jclid) {
-    logchannel = logchanneljcl;
-  } else {
-    try { 
-      message.channel.send('An error has happened, please report this to RandomGamer123 #5222 immediately');
-    }
-    catch(err) {
-    }
+      channelobj.send({embed: {
+        color: 16711680,
+        title: "Message Edited",
+        description: "Message sent by <@" + message.member.id +"> edited in <#" + message.channel.id + ">",
+        fields: [{
+            name: "Original Message:",
+            value: message.content
+          },
+          {
+            name: "Edited Message:",
+            value: emessage.content
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: "User id of original message sender: " + message.member.id
+        }
+    });
   }
-var channelobj = message.guild.channels.find("name", logchannel);
-  try {
-    channelobj.send({embed: {
-      color: 16711680,
-      title: "Message Edited",
-      description: "Message sent by <@" + message.member.id +"> edited in <#" + message.channel.id + ">",
-      fields: [{
-          name: "Original Message:",
-          value: message.content
-        },
-        {
-          name: "Edited Message:",
-          value: emessage.content
-        },
-      ],
-      timestamp: new Date(),
-      footer: {
-        text: "User id of original message sender: " + message.member.id
-      }
-    }
-  });
-}
-catch(err) {
-}
+ }
+ catch(err) {
+    message.channel.send('An error has happened, please report this to RandomGamer123 #5222 immediately');
+ }
 });
 client.login(token);
