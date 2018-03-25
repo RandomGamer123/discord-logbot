6622b460a762b862acbcd3040339b256e3ec7401
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2018-03-25 18:47:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const jclid = 214869811547734016;
 const logchannelramt = "modchat";
 const logchanneljcl = "log";
 const token = process.env.TOKEN;
+console.log(token);
 client.on('ready', () => {
   console.log(`Success!`);
 });
