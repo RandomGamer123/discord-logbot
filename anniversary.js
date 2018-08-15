c6d7a54f6629373854956ebfa97069b40d7fc863
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2018-08-15 23:00:56 +0200

    Update anniversary.js

--- anniversary.js
+++ anniversary.js
@@ -1,1 +1,15 @@
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const general = 235490833225220096;
+const token = process.env.TOKEN;
+console.log('Starting Up.');
+console.log(process.env.TOKEN);
+console.log(token);
+client.on('ready', () => {
+  console.log(`Success!`);
+  for x in client.channels.array() {
+    if (x.id == general) {
+      x.send('Happy  2nd anniversary!');
+    }
+  }
+});
